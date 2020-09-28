e0e68056a64d50d44a7822dc9532453cc6333458
jamescadigan97 <64744915+jamescadigan97@users.noreply.github.com>
2020-09-28 00:12:43 -0400

    select menu

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,8 +1,16 @@
 //Read Json file
     url = "../data/samples.json"
+    out_ids_list = []
 
     d3.json(url).then(function(data) {
-        console.log(data);
-    });
+        console.log(data)
 
-    const dataPromise = d3.json(url);
+        console.log(data.names)
+    d3.select("#selDataset")
+        .data(data.names)
+        .enter()
+        .append("option")
+        .text((function (d) {return d.names})
+        .attr("value", function (d) {return d.names})      
+//For data function
+    });
